ccd6db51cac18ec166733697e692616226af7bd0
mahesh-tippanu <102399228+mahesh-tippanu@users.noreply.github.com>
2022-07-14 22:08:04 +0530

    Add files via upload

---
+++ src/main/java/com/example/Employeepayroll/EmployeeDto.java
@@ -0,0 +1,49 @@
+package com.example.Dto;
+
+public class EmployeeDto {
+	    public String name;
+	    public String gender;
+	    public String department;
+	    public long salary;
+
+	    public EmployeeDto(String name, String gender, String department, long salary) {
+	        this.name = name;
+	        this.gender = gender;
+	        this.department = department;
+	        this.salary = salary;
+	    }
+
+	    public String getEmployee_name() {
+	        return name;
+	    }
+
+	    public void setEmployee_name(String employee_name) {
+	        this.name = employee_name;
+	    }
+
+	    public String getGender() {
+	        return gender;
+	    }
+
+	    public void setGender(String gender) {
+	        this.gender = gender;
+	    }
+
+	    public String getDepartment() {
+	        return department;
+	    }
+
+	    public void setDepartment(String department) {
+	        this.department = department;
+	    }
+
+	    public long getSalary() {
+	        return salary;
+	    }
+
+	    public void setSalary(long salary) {
+	        this.salary = salary;
+	    }
+	}
+
+
