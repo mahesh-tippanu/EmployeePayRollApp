c01700a920391173a7238f56773ac8de84030c28
mahesh-tippanu <102399228+mahesh-tippanu@users.noreply.github.com>
2022-07-14 21:55:18 +0530

    added Http Curd operation

--- src/main/java/com/example/Employeepayroll/EmployeeService.java
+++ src/main/java/com/example/Employeepayroll/EmployeeService.java
@@ -1,10 +1,41 @@
 package com.example.EmployeePayrollservice;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.Model.EmployeeDetails;
+import com.example.Repository.Repository;
 
 @Service
 public class EmployeeService {
-	public String Message(){
-        return "Hello World!";
-    }
+	 @Autowired
+	    Repository Repository;
+
+	    //Insert Details into DB Table
+	    public EmployeeDetails postDetails(EmployeeDetails employeeDetails){
+	        EmployeeDetails employeeDetails1=new EmployeeDetails(employeeDetails);
+	        Repository.save(employeeDetails1);
+	        return employeeDetails1;
+	    }
+
+	    //Display  Employee Details
+	    public Optional<EmployeeDetails> getDetails(Long id){
+	        return Repository.findById(id);
+	    }
+
+	    //Edit or Update Employee Details
+	    public EmployeeDetails putDetails(EmployeeDetails employeeDetails,Long id){
+	        EmployeeDetails employeeDetails1=new EmployeeDetails(employeeDetails,id);
+	        Repository.save(employeeDetails1);
+	        return employeeDetails1;
+	    }
+
+	    //Delete Employee Details
+	    public String deleteDetails(Long id){
+	        EmployeeDetails employeeDetails=new EmployeeDetails(id);
+	        Repository.delete(employeeDetails);
+	        return "record deleted";
+	    }
 }
--- src/main/java/com/example/Employeepayroll/Employypayrollcontroller.java
+++ src/main/java/com/example/Employeepayroll/Employypayrollcontroller.java
@@ -1,18 +1,49 @@
 package com.example.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.EmployeePayrollservice.EmployeeService;
+import com.example.Model.EmployeeDetails;
 
 @RestController
 public class Employypayrollcontroller {
 	@Autowired
 	EmployeeService employeeService;
 	
-	@GetMapping("/welcome")
-	public String displayMessage(){
-    return employeeService.Message();
-}
+	 //PostMapping Employee Details
+    @PostMapping("/postMap")
+    public EmployeeDetails postDetail(@RequestBody EmployeeDetails employeeDetails){
+        EmployeeDetails employeeDetails1=employeeService.postDetails(employeeDetails);
+        return employeeDetails1;
+    }
+
+    //GetMapping Employee Details
+    @GetMapping("/getMap/{id}")
+    public Optional<EmployeeDetails> getDetail(@PathVariable Long id){
+        Optional<EmployeeDetails> employeeDetails=employeeService.getDetails(id);
+        return employeeDetails;
+    }
+
+    //PutMapping Employee Details
+    @PutMapping("/putMap/{id}")
+    public EmployeeDetails putDetails(@RequestBody EmployeeDetails employeeDetails,@PathVariable Long id){
+        EmployeeDetails employeeDetails1=employeeService.putDetails(employeeDetails,id);
+        return employeeDetails1;
+    }
+
+    //Delete Employee Details
+    @DeleteMapping("/deleteMap/{id}")
+    public String deleteDetails(@PathVariable Long id){
+        String employeeDetails=employeeService.deleteDetails(id);
+        return "record deleted" ;
+    }
 }
